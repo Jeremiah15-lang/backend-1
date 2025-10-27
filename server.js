3853af14bdc42c93ba68f5e7c7249568a3cd2e8c
Jeremiah15-lang <olumidefisayomi15@gmail.com>
2025-10-27 23:37:53 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Backend is working')
+});
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log('MongoDB Error:', err))
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
